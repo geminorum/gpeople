68d421d6f2dd72723abad5ba6ae48a26ee0c5da9
Nasser Rafie <contact@geminorum.ir>
2016-05-06 12:37:26 +0430

    setting up gulp

---
+++ gulpfile.js
@@ -0,0 +1,107 @@
+(function() {
+	'use strict';
+
+	var
+		gulp = require('gulp'),
+		sass = require('gulp-sass'), // https://github.com/dlmanning/gulp-sass
+		changed = require('gulp-changed'),
+		// notify = require('gulp-notify'), // https://github.com/mikaelbr/gulp-notify
+		tinypng = require('gulp-tinypng'), // https://github.com/creativeaura/gulp-tinypng
+		nano = require('gulp-cssnano'), // https://github.com/ben-eb/gulp-cssnano
+		sourcemaps = require('gulp-sourcemaps'),
+		smushit = require('gulp-smushit'), // https://github.com/heldr/gulp-smushit
+		// pngquant = require('imagemin-pngquant'), // https://github.com/imagemin/imagemin-pngquant
+		excludeGitignore = require('gulp-exclude-gitignore'), // https://github.com/sboudrias/gulp-exclude-gitignore
+		wpPot = require('gulp-wp-pot'), // https://github.com/rasmusbe/gulp-wp-pot
+		sort = require('gulp-sort'),
+		fs = require('fs');
+
+	var
+		json = JSON.parse(fs.readFileSync('./package.json'));
+
+	gulp.task('tinypng', function() {
+
+		return gulp.src('./assets/images/raw/*.png')
+
+		.pipe(tinypng(''))
+
+		.pipe(gulp.dest('./assets/images'));
+	});
+
+	gulp.task('pngquant', function() {
+
+		return gulp.src('./assets/images/raw/*.png')
+
+		.pipe(pngquant({
+			quality: '65-80',
+			speed: 4
+		}))
+
+		.pipe(gulp.dest('./assets/images'));
+	});
+
+	gulp.task('smushit', function() {
+
+		return gulp.src('./assets/images/raw/**/*.{jpg,png}')
+
+		.pipe(smushit())
+
+		.pipe(gulp.dest('./assets/images'));
+	});
+
+	gulp.task('pot', function() {
+
+		return gulp.src([
+			'./**/*.php',
+			'!./assets/libs/**',
+			'!./gplugin/**',
+		])
+
+		.pipe(excludeGitignore())
+
+		.pipe(sort())
+
+		.pipe(wpPot(json._pot))
+
+		.pipe(gulp.dest('./languages'));
+	});
+
+	gulp.task('sass', function() {
+
+		return gulp.src('./assets/sass/**/*.scss')
+
+		.pipe(sourcemaps.init())
+
+		.pipe(sass().on('error', sass.logError))
+
+		.pipe(nano({
+			discardComments: {
+				removeAll: true
+			}
+		}))
+
+		.pipe(sourcemaps.write('./maps'))
+
+		.pipe(gulp.dest('./assets/css'));
+
+		//.pipe(livereload())
+
+		// .pipe(notify({
+		// 	message: "Sass Compiled."
+		// }));
+
+	});
+
+	gulp.task('watch', function() {
+
+		gulp.watch('./assets/sass/**/*.scss', [
+			'sass'
+		]);
+	});
+
+	gulp.task('default', function() {
+
+		console.log('Hi, I\'m Gulp!');
+	});
+
+}());
