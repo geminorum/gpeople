cffdb8294172932358cd21d48e0edc7846dde7a5
Nasser Rafie <contact@geminorum.ir>
2016-06-08 07:56:15 +0430

    using new gNetwork helper on overview

--- admin/remote.admin.overview.php
+++ admin/remote.admin.overview.php
@@ -1,4 +1,4 @@
 <?php defined( 'ABSPATH' ) or die( 'Restricted access' );
 
-if ( class_exists( 'gNetworkUtilities' ) )
-	gNetworkUtilities::githubREADME( 'geminorum/gpeople' );
+if ( function_exists( 'gnetwork_github_readme' ) )
+	gnetwork_github_readme( 'geminorum/gpeople' );
