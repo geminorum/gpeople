d3378849c37da194b4ac8847a1b976496c7023a9
Nasser Rafie <contact@geminorum.ir>
2016-08-09 03:59:49 +0430

    :wrench:  maintenance

--- includes/remotetemplate.class.php
+++ includes/remotetemplate.class.php
@@ -1,12 +1,5 @@
 <?php defined( 'ABSPATH' ) or die( 'Restricted access' );
 
-// http://wp-snippet.com/snippets/author-list-with-avatar-thumbnail/
-// http://vip.wordpress.com/documentation/incorporate-co-authors-plus-template-tags-into-your-theme/
-
-// http://codex.wordpress.org/Template_Tags/get_the_author
-// http://codex.wordpress.org/Function_Reference/the_author
-// http://codex.wordpress.org/Function_Reference/the_author_posts_link
-
 class gPeopleRemoteTemplate extends gPluginTemplateCore
 {
 
@@ -89,58 +82,6 @@ class gPeopleRemoteTemplate extends gPluginTemplateCore
 			}
 		}
 	}
-
-	// JUSTACOPY
-	// http://www.billerickson.net/wordpress-post-multiple-authors/
-	public static function person_box( $id = FALSE )
-	{
-		if ( ! $id )
-			return;
-
-		$authordata    = get_userdata( $id );
-		$gravatar_size = apply_filters( 'genesis_author_box_gravatar_size', 70, $context );
-		$gravatar      = get_avatar( get_the_author_meta( 'email', $id ), $gravatar_size );
-		$title         = apply_filters( 'genesis_author_box_title', sprintf( '<strong>%s %s</strong>', __( 'About', GPEOPLE_TEXTDOMAIN ), get_the_author_meta( 'display_name', $id ) ), $context );
-		$description   = wpautop( get_the_author_meta( 'description', $id ) );
-
-		/** The author box markup, contextual */
-		$pattern = $context == 'single' ? '<div class="author-box"><div>%s %s<br />%s</div></div><!-- end .authorbox-->' : '<div class="author-box">%s<h1>%s</h1><div>%s</div></div><!-- end .authorbox-->';
-
-		echo apply_filters( 'genesis_author_box', sprintf( $pattern, $gravatar, $title, $description ), $context, $pattern, $gravatar, $title, $description );
-
-	}
-
-	// JUSTACOPY
-	// http://www.billerickson.net/wordpress-post-multiple-authors/
-	public static function people_box()
-	{
-		if ( ! is_single() )
-			return;
-
-		if ( function_exists( 'get_coauthors' ) ) {
-			$authors = get_coauthors();
-			foreach ( $authors as $author )
-				self::person_box( $author->data->ID );
-		} else {
-			self::person_box( get_the_author_ID() );
-		}
-	}
-
-	// JUSTACOPY
-	// http://www.billerickson.net/wordpress-post-multiple-authors/
-	function be_post_authors_post_link_shortcode( $atts )
-	{
-		$atts = shortcode_atts( array(
-			'between'      => NULL,
-			'between_last' => NULL,
-			'before'       => NULL,
-			'after'        => NULL
-		), $atts );
-
-		$authors = function_exists( 'coauthors_posts_links' ) ? coauthors_posts_links( $atts['between'], $atts['between_last'], $atts['before'], $atts['after'], FALSE ) : $atts['before'] . get_author_posts_url() . $atts['after'];
-		return $authors;
-	}
-	//add_shortcode( 'post_authors_post_link', 'be_post_authors_post_link_shortcode' );
 }
 
 if ( ! function_exists( 'gpeople_byline' ) ) : function gpeople_byline( $post_id = NULL, $atts = array(), $cache = TRUE ) {
